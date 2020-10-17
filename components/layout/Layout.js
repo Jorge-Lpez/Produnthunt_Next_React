2b18651577cb3d21ece7720f20c6a778e431f1a2
jorge abascal lopez <jorgeabascal20@gmail.com>
2020-10-17 16:33:34 -0500

    error en git

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -61,7 +61,7 @@ const Layout = props => {
                <title>Product Hunt Firebase y Next.js</title>
                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" integrity="sha512-oHDEc8Xed4hiW6CxD7qjbnI+B07vDdX7hEPTvn9pSZO1bcRqHp8mj9pyr+8RVC2GmtEfI2Bi9Ke9Ass0as+zpg==" crossorigin="anonymous" />
                <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap" rel="stylesheet"/>
-                <link href="./app.css" rel="stylesheet"/>
+               <link href="./app.css" rel="stylesheet"/>
            </Head>
 
            <Header></Header>
